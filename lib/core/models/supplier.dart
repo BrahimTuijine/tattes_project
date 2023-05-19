18c2233d441ba4df89b297c55aaafd9af47d198f
brahim tuijine <brahimtuijine220@gmail.com>
2023-05-19 17:21:59 +0100

    fix email and number

--- lib/core/models/supplier.dart
+++ lib/core/models/supplier.dart
@@ -8,7 +8,7 @@ class Supplier {
     this.name = 'GRIBAA WAEL VENTE GROS PRODUITS \nALIMENTAIRES',
     this.address = "RUE 17 Decembre 7016 El ALIA",
     this.mf = "1795670 / J / N / C000",
-    this.phone = "20074624 - 54056468",
-    this.email = "waelg@gmail.com",
+    this.phone = "20074624 - 99327336",
+    this.email = "waelgribaa5@gmail.com",
   });
 }
